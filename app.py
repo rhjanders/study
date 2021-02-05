c530d2537aa178ebd5b86c09e09119eaf4dcac52
Jacob Anders <janders@redhat.com>
2021-02-05 11:54:21 +1000

    Updated webserver message.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server running on " +  myaddr  + " .</p>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an updated example web server running on " +  myaddr  + " .</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":
